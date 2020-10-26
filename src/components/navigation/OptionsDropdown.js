6099dc25e78fb150bcf9108925886f8e3c208f14
Arash-11 <arash.nawy@gmail.com>
2020-10-26 08:04:38 -0400

    completed styling for login and signup pages

--- src/components/navigation/OptionsDropdown.js
+++ src/components/navigation/OptionsDropdown.js
@@ -38,9 +38,9 @@ function OptionsDropdown() {
         <>
             { isOpen && <PasswordModal closeModal={controlModalDisplay} /> }
             <div className="options-modal">
-                <button onClick={logOut}>Log out</button>
-                <button onClick={controlModalDisplay}>Change password</button>
-                <button onClick={deleteAccount}>Delete account</button>
+                <button onClick={logOut} className="options-modal__btn">Log out</button>
+                <button onClick={controlModalDisplay} className="options-modal__btn">Change password</button>
+                <button onClick={deleteAccount} className="options-modal__btn">Delete account</button>
             </div>
         </>
     );
